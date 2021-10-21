aab4ff1754893459b9f98929bac19cb00c617b73
nerilka <nerilka.ann@gmail.com>
2021-10-21 16:01:46 +0300

    second step: compile and command ./gradlew

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,8 +1,9 @@
 package hexlet.code;
 
 public class App {
-	public static void main(String[] args) {
-	System.out.println("Hello World!");
-	}
+    public static void main(String[] args) {
+
+        System.out.println("Welcom to the Brain Games!");
+    }
 }
 
